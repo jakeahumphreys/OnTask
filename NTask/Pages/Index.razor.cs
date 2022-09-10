310c742090252a0d233f1aa17e91df251d0a0630
jakeahumphreys <jakehumphreys@outlook.com>
2022-09-10 23:57:30 +0100

    Will be removing this, just proving the database is working

--- NTask/Pages/Index.razor.cs
+++ NTask/Pages/Index.razor.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Components;
 using NTask.Data.Repositories;
-using NTask.Data.Services;
 
 namespace NTask.Pages;
 
@@ -8,8 +7,14 @@ public partial class Index
 {
     [Inject] private IProjectRepository _projectRepository { get; set; }
     
+    private int ProjectCount { get; set; }
+    
     protected override void OnInitialized()
     {
-        
+        //TODO remove
+        //Right now this is a hacky test and will be removed ASAP
+        //shouldn't be directly using the repo for this.
+        var projects = _projectRepository.GetAll();
+        ProjectCount = projects.Count;
     }
 }
