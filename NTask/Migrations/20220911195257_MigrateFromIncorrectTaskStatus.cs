929b36bd832f1fc69617f93c6ceff309ccda553b
jakeahumphreys <jakehumphreys@outlook.com>
2022-09-11 20:53:37 +0100

    It wasn't needed, but I wanted to migrate after realising TaskStatus was wrong

---
+++ NTask/Migrations/20220911195257_MigrateFromIncorrectTaskStatus.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NTask.Migrations
+{
+    public partial class MigrateFromIncorrectTaskStatus : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
