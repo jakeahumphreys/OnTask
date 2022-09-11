05b49078e30bd0e9904f45571d504cef5fbafdc8
jakeahumphreys <jakehumphreys@outlook.com>
2022-09-11 15:41:06 +0100

    Migrating the UI to MudBlazor

--- NTask/Program.cs
+++ NTask/Program.cs
@@ -1,3 +1,4 @@
+using MudBlazor.Services;
 using NTask.Data.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddMudServices();
 builder.Services.AddSingleton<IProjectRepository, ProjectRepository>();
 
 var app = builder.Build();
