00f97aad642d22e5cddb7685f86e33231437b9a2
jakeahumphreys <jakehumphreys@outlook.com>
2022-09-11 13:22:52 +0100

    Removed hacky test code

--- NTask/Pages/Index.razor.cs
+++ NTask/Pages/Index.razor.cs
@@ -1,20 +1,20 @@
 using Microsoft.AspNetCore.Components;
 using NTask.Data.Repositories;
+using NTask.Data.Services;
 
 namespace NTask.Pages;
 
 public partial class Index
 {
-    [Inject] private IProjectRepository _projectRepository { get; set; }
+    [Inject] private IProjectRepository ProjectRepository { get; set; }
     
     private int ProjectCount { get; set; }
     
     protected override void OnInitialized()
     {
-        //TODO remove
-        //Right now this is a hacky test and will be removed ASAP
-        //shouldn't be directly using the repo for this.
-        var projects = _projectRepository.GetAll();
-        ProjectCount = projects.Count;
+        var projectService = new ProjectService(ProjectRepository);
+        
+        var projectResponse = projectService.GetAllProjects();
+        ProjectCount = projectResponse.Projects.Count;
     }
 }
